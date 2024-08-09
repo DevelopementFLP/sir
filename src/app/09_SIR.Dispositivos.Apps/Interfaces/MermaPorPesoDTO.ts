b5f82298b9e82cdfa2094be6cf89276b0c3065d1
RperaltaFLP <rperalta@flp.uy>
2024-08-09 11:28:39 -0300

    Actualizacion 9/8/24 11:28

---
+++ src/app/09_SIR.Dispositivos.Apps/Interfaces/MermaPorPesoDTO.ts
@@ -0,0 +1,13 @@
+export interface MermaPorPesoDTO {
+  fechaDeBalanza: string;
+  fechaDeInnova: string;
+  carcassID: string;
+  ladoAnimal: string;
+  diferenciadePeso: string;
+  pesoInnova: number;
+  pesoLocal: number;
+  porsentajeDeMerma: number;
+  etiqueta: string;
+  tropa: string;
+  proveedor: string;
+}
