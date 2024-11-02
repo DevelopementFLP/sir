b95dbdbb412b6f3e7435d12cc578c5a8f364aa6f
RperaltaFLP <rperalta@flp.uy>
2024-11-02 10:34:46 -0300

    Actualizacion Ficha Tectina

--- src/app/10_Sir.Faena.Apps/components/body-reporte-resumen-de-cuarteo/body-reporte-resumen-de-cuarteo.component.ts
+++ src/app/10_Sir.Faena.Apps/components/body-reporte-resumen-de-cuarteo/body-reporte-resumen-de-cuarteo.component.ts
@@ -41,7 +41,7 @@ export class BodyReporteMediasComponent {
   }
   public generarListaDeHasta(): void {
     this.listaDeHorasHasta = [];
-    for (let i = 4; i <= 12; i += 2) {
+    for (let i = 0; i < 8; i += 2) {
       this.listaDeHorasHasta.push(i.toString().padStart(2, '0'));
     }
   }
